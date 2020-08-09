3eb79c105db4d7ae5acec86bcf474317c6cde919
soon <69294741+vlyly@users.noreply.github.com>
2020-08-09 17:11:44 +0900

    Add files via upload

--- clock.js
+++ clock.js
@@ -7,14 +7,17 @@ function getTime(){
     const hours = date.getHours();
     const seconds = date.getSeconds();
 
-    clockTitle.innerText = `${hours}:${hours}:${minutes}`; //clockTitle.innerText = hours + ":" + minutes + ":" + seconds;
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 10 ? `0${seconds}` : seconds }`; //=clockTitle.innerText = hours +gi ":" + minutes + ":" + seconds; 시, 분, 초가 각각 10 이하의 수 일 때 0X의 형식으로 표기합니다.
 
 }
 
 function init(){
 
     getTime();
-
+    setInterval(getTime, 1000); // getTime함수를 1초에 한 번 씩 실행합니다. (1초마다 업데이트)
 }
 
 init();
